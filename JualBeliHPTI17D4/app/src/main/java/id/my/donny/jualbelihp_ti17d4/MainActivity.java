60bac4dd15d8c3a50ae9d38b16167399140c1463
rendyrenaldhy <renaldhyrendy06@gmail.com>
2020-04-05 00:59:04 +0700

    project mandiri

---
+++ JualBeliHPTI17D4/app/src/main/java/id/my/donny/jualbelihp_ti17d4/MainActivity.java
@@ -0,0 +1,14 @@
+package id.my.donny.jualbelihp_ti17d4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
